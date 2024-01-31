4861d11e4306278597b87dc41b679f24f666cd9e
Marine Van Hees <vanheesmarine@gmail.com>
2024-01-31 11:07:37 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/home'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
