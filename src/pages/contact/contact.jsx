5ea238d05e1fa4032f2cdb21150c5c6d541490c0
Marine Van Hees <vanheesmarine@gmail.com>
2024-01-31 16:24:07 +0100

    pages

---
+++ src/pages/contact/contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/navbar/navbar";
+
+const Contact = () => {
+ return (
+    <main className="contact w-full h-full">
+        <Navbar />
+    </main>
+ );
+};
+
+export default Contact;
---
+++ src/pages/projects/projects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/navbar/navbar";
+
+const Projects= () => {
+ return (
+    <main className="projects w-full h-screen">
+        <Navbar />
+    </main>
+ );
+};
+
+export default Projects;
